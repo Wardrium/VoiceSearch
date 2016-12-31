7153b5e025ec9213f80f38017c3371a90869fc52
Ward Chiang <wardrium@gmail.com>
2016-12-30 19:49:41 -0500

    Changed content script to be injected into page properly instead of being included in popup.html. Added function to navigation.js that will open a specified video from youtube homepage.

--- navigation.js
+++ navigation.js
@@ -1,11 +1,15 @@
-function navigate(URL){
-	chrome.tabs.update({
-		url: "http://www.youtube.com/"
-	});
+// Returns what page on youtube user is at. 0 = home, 1 = search, -1 = none of the above
+function get_page(){
+	
+}
+
+function view_video(index){
+	var link = document.getElementsByClassName(" yt-ui-ellipsis yt-ui-ellipsis-2 yt-uix-sessionlink       spf-link ");
+	link[index].click();
 }
 
 chrome.runtime.onMessage.addListener(
 	function(request, sender, sendResponse){
-		navigate(request.navigate);
+		view_video(request.view);
 	}
 );
--- popup.js
+++ popup.js
@@ -1,5 +1,13 @@
 document.addEventListener('DOMContentLoaded', function(){
 	document.getElementById('youtube').addEventListener('click', function(){
-		chrome.runtime.sendMessage({navigate: 'http://www.youtube.com'})
+		chrome.tabs.update({
+			url: "http://www.youtube.com/"
+		});
+		chrome.tabs.executeScript(null, {file: "navigation.js"});
+	});
+	document.getElementById('first').addEventListener('click', function(){
+		chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+			chrome.tabs.sendMessage(tabs[0].id, {view: '0'});
+		});
 	});
 })

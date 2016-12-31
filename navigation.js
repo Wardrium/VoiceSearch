b5b1d36ecb68a14b1e84584d00526a637ac3a510
Ward Chiang <wardrium@gmail.com>
2016-12-30 23:20:12 -0500

    Can now navigate through the sidebar.

--- navigation.js
+++ navigation.js
@@ -1,11 +1,23 @@
-var URLs = [];	// Array of URLs for navigation.
+// Index of each element = number it is visually assigned
+var video_URLs = [];
+var sidebar_URLs = [];
 
 $(document).ready(function(){
+	// Number the videos on the home page.
 	var counter = 0;
 	$(".yt-ui-ellipsis.yt-ui-ellipsis-2.yt-uix-sessionlink.spf-link").each(function(){
 		var name = $(this).text();
 		$(this).text(counter + ': ' + name);
-		URLs.push($(this).attr('href'));
+		video_URLs.push($(this).attr('href'));
+		counter += 1;
+	});
+
+	// Number the subscriptions on the sidebar.
+	counter = 0;
+	$(".guide-item.yt-uix-sessionlink.yt-valign.spf-link").each(function(){
+		var name = $(this).text();
+		$(this).text(counter + ': ' + name);
+		sidebar_URLs.push($(this).attr('href'));
 		counter += 1;
 	});
 });
@@ -15,12 +27,19 @@ function get_page(){
 
 }
 
-function view_video(index){
-	window.location.href = URLs[index];
+function navigate_video(index){
+	window.location.href = video_URLs[index];
+}
+
+function navigate_sidebar(index){
+	window.location.href = sidebar_URLs[index];
 }
 
 chrome.runtime.onMessage.addListener(
 	function(request, sender, sendResponse){
-		view_video(request.view);
+		if (request.view_video)
+			navigate_video(request.view_video);
+		else if (request.view_sidebar)
+			navigate_sidebar(request.view_sidebar);
 	}
 );
--- popup.js
+++ popup.js
@@ -4,9 +4,14 @@ document.addEventListener('DOMContentLoaded', function(){
 			url: "http://www.youtube.com/"
 		});
 	});
-	document.getElementById('first').addEventListener('click', function(){
+	document.getElementById('video').addEventListener('click', function(){
 		chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-			chrome.tabs.sendMessage(tabs[0].id, {view: '0'});
+			chrome.tabs.sendMessage(tabs[0].id, {view_video: document.getElementById('video_index').value});
+		});
+	});
+	document.getElementById('sidebar').addEventListener('click', function(){
+		chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+			chrome.tabs.sendMessage(tabs[0].id, {view_sidebar: document.getElementById('sidebar_index').value});
 		});
 	});
 })

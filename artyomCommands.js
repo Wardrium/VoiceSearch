00fe702551668148d6d4b5ca8369893bda12e7ee
Ward Chiang <wardrium@gmail.com>
2016-12-31 13:34:21 -0500

    Artyom now works with voice commands. Only 'hello' works right now.

--- artyomCommands.js
+++ artyomCommands.js
@@ -1,61 +1,23 @@
- /**
-  * Artyom commands examples. Just add this file after artyom is loaded in your document.
-  * You can save all your commands in this file or simply follow the workflow :
-  *
-  * @example artyom.addCommands({CommandObject})
-  * @copyright Carlos Delgado 2016
-  * @author Carlos Delgado - www.ourcodeworld.com
-  * @param {type} window
-  * @see https://sdkcarlos.github.io/sites/artyom.html
-  * @returns Artyom
-  */
-(function(window){
-    'use strict';
-
-    /**
-     * Example Artyom Commands
-     * @type Array
-     */
-    var artyomCommands = [
-        //Simple Command Example
-        {
-            indexes: ['hello'],
-            action : function(i){
-                artyom.say("Hello, how are you? My name is Artyom.",{
-                    onStart: function(){
-                        console.log("Speaking presentation");
-                    },
-                    onEnd: function(){
-                        console.log("All that i've to say has been said.");
-                    }
-                });
-            }
-        },
-        //Smart Command Example
-        {
-            indexes: ['pronunciate * please'],
-            smart:true,
-            action : function(i,wildcard,recognized_text){
-                console.log("Recognized : " + recognized_text,"Wildcard : "+wildcard);
-                artyom.say(wildcard);
+artyom.addCommands([
+    {
+        indexes:["hello", "hey"],
+        action:function(i){
+            if (i == 0){
+                artyom.say("Hello! How are you?");
             }
         }
-        //Continue adding your own commands here
-    ];
+    }
+]);
 
-    artyom.addCommands(artyomCommands);
+function startArtyom(){
 
-    /**
-     * Or use the shorter and cleaner method :
-     */
+    artyom.initialize({
+        lang: "en-GB",
+        continuous: false,
+        debug: true,
+        listen: true
+    })
+    artyom.say('initialized');
+}
 
-    artyom.on(['Good morning']).then(function(i){
-        alert("Good morning ! How are you?");
-    });
-
-    artyom.on(['Repeat after me *'] , true).then(function(i, wildcard){
-        alert("You've said " + wildcard);
-    });
-
-    console.log(artyom.getAvailableCommands());
-})(window);
+startArtyom();

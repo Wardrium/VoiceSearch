e323cc40725eb1bee5190e022e84d8d92e85582e
Ward Chiang <wardrium@gmail.com>
2017-01-05 22:20:05 -0500

    Can adjust video volume with voice commands.

--- handlers/video.js
+++ handlers/video.js
@@ -3,33 +3,39 @@
 // Voice Commands----------------------------------------------------
 artyom.addCommands([
     {
-        indexes:["paws", "pause", "resume", "play", "rewind", "skip", "restart", "lower volume", "increase volume"],
+        indexes:["paws", "pause", "resume", "play", "rewind", "skip", "restart", "lower volume", "increase volume", "mute", "unmute"],
         action:function(cmd){
             if (cmd == 0 || cmd == 1){ // Pause. Artyom interprets my 'pause' as 'paws'.
                 artyom.say("Pausing video");
-                nav.pause_video();
+                vid.pause_video();
             }
             else if (cmd == 2 || cmd == 3){ // Resume/play
                 artyom.say("Resuming video");
-                nav.resume_video();
+                vid.resume_video();
             }
             else if (cmd == 4){ // Rewind 5 seconds
                 artyom.say("Rewinding 10 seconds");
-                nav.set_timer(nav.get_timer() - 10);
+                vid.set_timer(nav.get_timer() - 10);
             }
             else if (cmd == 5){ // Skip 5 seconds
                 artyom.say("Skipping 10 seconds");
-                nav.set_timer(nav.get_timer() + 10);
+                vid.set_timer(nav.get_timer() + 10);
             }
             else if (cmd == 6){ // Restart video
                 artyom.say("Restarting video");
-                nav.set_timer(0);
+                vid.set_timer(0);
             }
-            else if (cmd == 7){
-
+            else if (cmd == 7){ // Lower volume
+                vid.change_volume(-0.2);
             }
-            else if (cmd == 8){
-                
+            else if (cmd == 8){ // Increase volume
+                vid.change_volume(0.2);
+            }
+            else if (cmd == 9){ // Mute volume
+                vid.mute_video();
+            }
+            else if (cmd == 10){ // Unmute volume
+                vid.unmute_video();
             }
         }
     }
@@ -43,7 +49,7 @@ artyom.addCommands([
             if (cmd == 0, 1){  // rewind. Sometimes artyom interprets rewind as remind.
                 if (Number(index)){ // insure index is a number
                     artyom.say("Rewinding " + index + " seconds")
-                    nav.set_timer(nav.get_timer() - index);
+                    vid.set_timer(nav.get_timer() - index);
                 }
                 else {
                     artyom.say("Did not understand rewind command")
@@ -52,7 +58,7 @@ artyom.addCommands([
             else if (cmd == 2){ // skip
                 if (Number(index)){
                     artyom.say("Skipping " + index + " seconds")
-                    nav.set_timer(nav.get_timer() + index);
+                    vid.set_timer(vid.get_timer() + index);
                 }
                 else {
                     artyom.say("Did not understand skip command")
@@ -64,23 +70,41 @@ artyom.addCommands([
 
 // Navigation--------------------------------------------------------
 var video = $("video")[0];
+vid = {};
 
-nav.pause_video = function(){
+vid.pause_video = function(){
     video.pause();
 }
 
-nav.resume_video = function(){
+vid.resume_video = function(){
     video.play();
 }
 
-nav.set_timer = function(newTime){
+vid.set_timer = function(newTime){
     video.pause();
     video.currentTime = newTime;
     video.play();
 }
 
-nav.get_timer = function(){
+vid.get_timer = function(){
     return video.currentTime;
+}
+
+vid.change_volume = function(vol){
+    var new_vol = video.volume + vol;
+    if (new_vol > 1)
+        new_vol = 1;
+    if (new_vol < 0)
+        new_vol = 0;
+    video.volume = new_vol;
+}
+
+vid.mute_video = function(){
+    video.muted = true;
+}
+
+vid.unmute_video = function(){
+    video.muted = false;
 }
 
 // DOM modification--------------------------------------------------
